923d70d31584e48a2939c7ee5f9f11fb5b2c7e6b
kavya <jkavya865@gmail.com>
2025-11-05 21:20:13 +0530

    Commit - Mid-Sems

--- Python - Amit Sir/Mid-Sem/BFS.py
+++ Python - Amit Sir/Mid-Sem/BFS.py
@@ -1,1 +1,36 @@
-graph
+# creating a graph first
+graph = {
+    '5' : ['3','7'],
+    '3' : ['2','4'],
+    '2' : [],
+    '4' : ['8'],
+    '7' : ['8'],
+    '8' : []
+}
+
+# Lisiting visited nodes
+visited = []
+
+# Intializing queue
+queue = []
+
+def bfs (visited,graph,node) :
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        m = queue.pop(0)
+        print(m, end = " ")
+
+
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+# Driver code
+print ("Following is the BFS Code : ")
+bfs(visited,graph,'5')
+
+
+
